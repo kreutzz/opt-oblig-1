4a1011d8e88d3d6ce950c458bbd6c7381aeaa5fc
kreutzz <adrian93kreutz@gmail.com>
2017-09-05 14:03:13 +0200

    Add files via upload

--- Opt_Oblig_1.java
+++ Opt_Oblig_1.java
@@ -15,12 +15,14 @@ public class Opt_Oblig_1{
 		ArrayList<Integer> route = new ArrayList<Integer>();
 
 		fillArray(n, city_graph);
-		//fillArray(n, connections);
 		printArray(n, city_graph);
 		randomMethod(n, connections, city_graph, route);
 		printArray(n, route);
 		printArray(n, connections);
-		calculateCost(n, route, city_graph);
+		System.out.println("kostnaden for random er: " + calculateCost(n, route, city_graph));
+		fillArray(n, connections);
+		iterativeRandomMethod(n, connections, city_graph, route);
+
 	}
 
 	public static void randomMethod(int n, int[] connections, int[][] array, ArrayList<Integer> route){
@@ -42,14 +44,52 @@ public class Opt_Oblig_1{
 		}
 	}
 
-	public static void calculateCost(int n, ArrayList<Integer> route, int[][] array){
+	public static void iterativeRandomMethod(int n, int[] connections, int[][] array, ArrayList<Integer> route) {
+		Random random = new Random();
+		int random_city;
+		int cost = 0;
+		int temp_cost = 0;
+		int iterator = 0;
+		ArrayList<Integer> temp_route = new ArrayList<Integer>();
+
+		while (iterator != 10) {
+			random_city = random.nextInt(n);
+			connections[random_city] = 1;
+			int counter = n-1;
+
+			while (counter != 0) {
+				random_city = random.nextInt(n);
+				if (connections[random_city] == 0) {
+					System.out.println("nei");
+					connections[random_city] = 1;
+					temp_route.add(random_city);
+					counter--;
+				}
+				System.out.println("hei");
+			}
+			System.out.println("hello");
+			temp_cost = calculateCost(n, temp_route, array);
+
+			//if (cost != 0 && temp_cost < cost) {
+			//	cost = temp_cost;
+			//	iterator = 0;
+			//}
+			//else if(cost == 0)
+			cost = temp_cost;
+			//else
+			iterator++;
+		}
+		printArray(n, temp_route);
+		System.out.println("kostnaden for Iterativ random er: " + cost);
+	}
+
+	public static int calculateCost(int n, ArrayList<Integer> route, int[][] array){
 		int cost = 0;
 
-		for(int i = 0; i < n -1; i++){
+		for(int i = 0; i < n - 1; i++){
 			cost = cost + array[route.get(i)][route.get(i+1)];
 		}
-
-		System.out.println(cost);
+		return cost;
 	}
 
 	public static void printArray(int n, int[] connections){
@@ -68,6 +108,11 @@ public class Opt_Oblig_1{
 				System.out.print(array[i][j] + " ");
 			System.out.println();
 		}
+	}
+
+	public static void fillArray(int n, int[] array){
+		for(int i = 0; i < n ; i++)
+			array[i] = 0;
 	}
 
 	public static void fillArray(int n, int[][] array){
