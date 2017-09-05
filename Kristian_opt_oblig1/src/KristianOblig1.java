1ba9948a2071ac8418d81deca569cce61afeeddb
Kristian Linna <kristianlinna@Kristians-MacBook-Pro-2.local>
2017-09-05 12:38:37 +0200

    Lagt til mappe for min kode

---
+++ Kristian_opt_oblig1/src/KristianOblig1.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+import java.util.ArrayList;
+import java.util.Random;
+
+public class KristianOblig1 {
+
+    public static void main(String[] args){
+
+        Scanner read = new Scanner(System.in);
+        int numberOfCities;
+        boolean[] isVisited;
+        int[] cities;
+
+        System.out.println("Enter number of cities:");
+        numberOfCities = read.nextInt();
+
+        isVisited = new boolean[numberOfCities];
+        cities = new int[numberOfCities];
+
+
+
+    }
+}
