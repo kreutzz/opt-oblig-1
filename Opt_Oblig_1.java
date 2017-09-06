fc94fcb09e3f536ee65b589f1f6fef93fa780ef6
kreutzz <adrian93kreutz@gmail.com>
2017-09-06 13:46:55 +0200

    Opt_Oblig_1

--- Opt_Oblig_1.java
+++ Opt_Oblig_1.java
@@ -13,28 +13,30 @@ public class Opt_Oblig_1{
 		int[][] city_graph = new int[n][n];
 		int[] connections = new int[n];
 		ArrayList<Integer> route = new ArrayList<Integer>();
+		ArrayList<Integer> randomRoute = new ArrayList<Integer>();
+		ArrayList<Integer> iterativeRandomRoute = new ArrayList<Integer>();
+		ArrayList<Integer> greedyRoute = new ArrayList<Integer>();
+
 
 		fillArray(n, city_graph);
+		fillArray(n, connections);
 		printArray(n, city_graph);
 		randomMethod(n, connections, city_graph, route);
 		printArray(n, route);
 		printArray(n, connections);
-		System.out.println("kostnaden for random er: " + calculateCost(n, route, city_graph));
+		System.out.println("kostnaden for Random er: " + calculateCost(n, route, city_graph));
 		fillArray(n, connections);
 		iterativeRandomMethod(n, connections, city_graph, route);
-
 	}
 
 	public static void randomMethod(int n, int[] connections, int[][] array, ArrayList<Integer> route){
-		
+		route.clear();
 		Random random = new Random();
 		int random_city = random.nextInt(n);
 		connections[random_city] = 1;
 		int counter = n-1;
 		route.add(random_city);
-
 		while(counter != 0){
-
 			random_city = random.nextInt(n);
 			if(connections[random_city] == 0){
 				connections[random_city] = 1;
@@ -45,50 +47,52 @@ public class Opt_Oblig_1{
 	}
 
 	public static void iterativeRandomMethod(int n, int[] connections, int[][] array, ArrayList<Integer> route) {
-		Random random = new Random();
-		int random_city;
+		route.clear();
+		fillArray(n, connections);
 		int cost = 0;
 		int temp_cost = 0;
 		int iterator = 0;
 		ArrayList<Integer> temp_route = new ArrayList<Integer>();
+		int[][] saved_routes;
 
-		while (iterator != 10) {
-			random_city = random.nextInt(n);
-			connections[random_city] = 1;
-			int counter = n-1;
-
-			while (counter != 0) {
-				random_city = random.nextInt(n);
-				if (connections[random_city] == 0) {
-					System.out.println("nei");
-					connections[random_city] = 1;
-					temp_route.add(random_city);
-					counter--;
-				}
-				System.out.println("hei");
-			}
-			System.out.println("hello");
+		while (iterator != 5){
+			fillArray(n,connections);
+			randomMethod(n, connections, array, temp_route);
 			temp_cost = calculateCost(n, temp_route, array);
-
-			//if (cost != 0 && temp_cost < cost) {
-			//	cost = temp_cost;
-			//	iterator = 0;
-			//}
-			//else if(cost == 0)
-			cost = temp_cost;
-			//else
+			
+			if(temp_cost < cost){
+				route.clear();
+				cost = temp_cost;
+				for(int i = 0; i < n ; i++){
+					route.add(temp_route.get(i));
+				}
+				fillArray(n, connections);
+			}
+			else if(cost == 0){
+				cost = temp_cost;
+				for(int i = 0; i < n ; i++){
+					route.add(temp_route.get(i));
+				}
+			}
 			iterator++;
+			printArray(n,route);
+			temp_route.clear();
 		}
-		printArray(n, temp_route);
 		System.out.println("kostnaden for Iterativ random er: " + cost);
+	}
+
+	public static void greedyMethod(){
+		
+		
 	}
 
 	public static int calculateCost(int n, ArrayList<Integer> route, int[][] array){
 		int cost = 0;
-
-		for(int i = 0; i < n - 1; i++){
+		
+		for(int i = 0; i < n -1; i++){
 			cost = cost + array[route.get(i)][route.get(i+1)];
 		}
+		
 		return cost;
 	}
 
@@ -98,8 +102,15 @@ public class Opt_Oblig_1{
 	}
 
 	public static void printArray(int n, ArrayList<Integer> route){
-		for(int i = 0; i < n; i++)
+		for(int i = 0; i < n; i++){
+			if(i == 0)
+				System.out.println("\nby " + route.get(i) + " til ");
+			else if(i==n-1)
+				System.out.println("by " + route.get(i) + "\n");
+			else
 			System.out.println("by " + route.get(i) + " til ");
+
+		}
 	}
 
 	public static void printArray(int n, int[][] array){
